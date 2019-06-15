732c50c224fb5bdbd62bfd34d662d7c7e4c47094
Todd Igoe <vujadetech42@gmail.com>
2019-06-15 01:00:47 -0400

    Add last and fall asleep at my desk

--- the-little-typer/misc.rkt
+++ the-little-typer/misc.rkt
@@ -659,23 +659,33 @@
   (λ (E es) (head es)))
 ;(base-last Nat vec-42)
 
-
+; NB some weird stuff: U since it's mapping types
 (claim mot-last (-> U Nat U))
-#;(define mot-last
-  (λ (E)
-    (λ (k) E)))
+(define mot-last
+  (λ (E k)
+    (-> (Vec E (add1 k)) E)))
+;(mot-last Atom 1)
+
+(claim step-last
+  (Π ((E U)
+      (l-1 Nat))
+    (-> (mot-last E l-1)
+      (mot-last E (add1 l-1)))))
+(define step-last
+  (λ (E l-1 last_l-1 es)
+    (last_l-1 (tail es))))
 
 (claim last
   (Π ((E U)
       (l Nat))
       (-> (Vec E (add1 l))
     E)))
-#;(define last
-  (λ (E l es)
+(define last
+  (λ (E l)
     (ind-Nat  l
-              (λ (k) E)
-              (base-last E (the (Vec E 1) es))
-              (λ (l-1 acc) (tail acc)))))
+              (mot-last E)
+              (base-last E)
+              (step-last E))))
 
 ; step-peas*, step-peas without arrows/arrowless
 #;(claim step-peas*
